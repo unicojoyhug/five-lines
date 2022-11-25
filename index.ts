17d0426841d012aa43acfeea81f3120262a030c9
Juyoung Choi <juyoung.choi@lego.com>
2022-11-25 13:08:14 +0100

    updateTile - step 3

--- index.ts
+++ index.ts
@@ -102,6 +102,7 @@ function updateMap(){
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
       // map & Tile
+      updateTile()
       if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
           && map[y + 1][x] === Tile.AIR) {
         map[y + 1][x] = Tile.FALLING_STONE;
