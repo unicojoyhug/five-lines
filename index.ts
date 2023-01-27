1351271058930cb09c52e44cd9cab6a307e35b87
Juyoung Choi <juyoung.choi@lego.com>
2023-01-27 13:48:11 +0100

    remove drawTile and inline to use draw

--- index.ts
+++ index.ts
@@ -484,15 +484,9 @@ function createCanvas() {
 function drawMap(g: CanvasRenderingContext2D) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      drawTile(g, x, y);
+      map[y][x].draw(g, x, y);
     }
   }
-}
-
-function drawTile(g: CanvasRenderingContext2D, x: number, y: number) {
-  map[y][x].color(g);
-  if (!map[y][x].isAir() && !map[y][x].isPlayer())
-    g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
 
 function drawPlayer(g: CanvasRenderingContext2D) {
